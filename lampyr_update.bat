4e53610bcb746787811c74f67e331831040ea9f5
Maxwell Madden <135063494+mxwllmadden@users.noreply.github.com>
2026-06-12 14:26:13 -0400

    Script Update

--- lampyr_update.bat
+++ lampyr_update.bat
@@ -1,9 +1,8 @@
 @echo off
 
 call conda activate
-call conda activate mx_hardware
-echo Updating conda environment from mx_hardware.yaml...
-call conda env update --name mx_hardware --file "N:\Maxwell\Lampyr\mx_hardware.yaml" --prune
+call conda activate lampyr
+call conda env update --name mx_hardware --f https://raw.githubusercontent.com/mxwllmadden/Lampyr/main/mx_hardware.yaml --prune
 pip install art
 pip uninstall lampyr -y
 pip install --no-deps "git+https://github.com/mxwllmadden/Lampyr.git@main"
